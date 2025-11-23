265ba57287fa8297bba29614861af9f833895629
Ahmet Kilinc <akx9@icloud.com>
2025-11-23 23:07:17 +0000

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "https://x.com/bruvimtired",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="min-h-screen w-full flex-col text-center p-16 bg-white dark:bg-black">
-      <p className="text-xl font-mono">bruvimtired browser - coming soon.</p>
       <div className="flex flex-col items-center justify-center gap-2 mt-4">
         <p>
           <Link href="https://x.com/bruvimtired">X (Twitter)</Link>
